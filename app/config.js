a0b32e240b6b3fca0747fb092ec2c24b8c2c6bcd
FabVillegas <f.villegas93@gmail.com>
2015-07-29 22:30:51 -0500

    Ajustes hechos

--- app/config.js
+++ app/config.js
@@ -2,14 +2,15 @@ angular.module('kitchen-time', [
   'ui.router',
   'pc035860.scrollWatch',
   'ngAnimate',
-  'sideWindow'
+  'sideWindow',
+  'uiGmapgoogle-maps'
 ]).
 config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('home');
 	$stateProvider.
 	state('home', {
 		url: '/home',
-		templateUrl: 'states/home/homeView-surrogate.html',
+		templateUrl: 'states/home/homeView.html',
 		controller: 'homeController'
 	});
 }]);
--- app/states/home/homeController.js
+++ app/states/home/homeController.js
@@ -1,17 +1,36 @@
 angular.module( 'kitchen-time' ).controller( 'homeController', homeController );
 
-homeController.$inject = [ '$scope' ];
+homeController.$inject = [ '$scope', '$timeout' ];
 
-function homeController( $scope ) {
-  var vm = this;
+function homeController( $scope, $timeout ) {
 
-  ///////////////////////////////////////////////////////
+  $scope.map = { center: { latitude: 25.656681, longitude: -100.36575 }, zoom: 19 };
+  $scope.options = { scrollwheel: false };
+  $scope.coordsUpdates = 0;
+  $scope.dynamicMoveCtr = 0;
+  $scope.marker = {
+    id: 0,
+    coords: {
+      latitude: 25.656681,
+      longitude: -100.36575
+    },
+    options: { draggable: true },
+    events: {
+      dragend: function (marker, eventName, args) {
+        $log.log('marker dragend');
+        var lat = marker.getPosition().lat();
+        var lon = marker.getPosition().lng();
+        $log.log(lat);
+        $log.log(lon);
 
-  angular.forEach( [ 1, 2, 3, 4, 5, 6, 7 ], function( i ) {
-    $scope.$on('slide' + i, onSlideChange);
-  });
-
-  function onSlideChange( $evt, active, locals ) {
-    $scope[$evt.name] = active;
+        $scope.marker.options = {
+          draggable: true,
+          labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+          labelAnchor: "100 0",
+          labelClass: "marker-labels"
+        };
+      }
+    }
   };
+
 };
