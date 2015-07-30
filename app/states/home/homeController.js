5e87d39ce5aacb2247eb49457f297e00427cad68
FabVillegas <f.villegas93@gmail.com>
2015-07-29 23:01:24 -0500

    Agregue el mapa

--- app/states/home/homeController.js
+++ app/states/home/homeController.js
@@ -5,32 +5,29 @@ homeController.$inject = [ '$scope', '$timeout' ];
 function homeController( $scope, $timeout ) {
 
   $scope.map = { center: { latitude: 25.656681, longitude: -100.36575 }, zoom: 19 };
+
   $scope.options = { scrollwheel: false };
-  $scope.coordsUpdates = 0;
-  $scope.dynamicMoveCtr = 0;
+
   $scope.marker = {
     id: 0,
     coords: {
       latitude: 25.656681,
       longitude: -100.36575
     },
-    options: { draggable: true },
-    events: {
-      dragend: function (marker, eventName, args) {
-        $log.log('marker dragend');
-        var lat = marker.getPosition().lat();
-        var lon = marker.getPosition().lng();
-        $log.log(lat);
-        $log.log(lon);
+  };
 
-        $scope.marker.options = {
-          draggable: true,
-          labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-          labelAnchor: "100 0",
-          labelClass: "marker-labels"
-        };
-      }
-    }
+  $scope.windowOptions = { visible: true };
+
+  $scope.window = {
+    template: './states/home/maps-template.html',
+  };
+
+  $scope.onClick = function() {
+      $scope.windowOptions.visible = !$scope.windowOptions.visible;
+  };
+
+  $scope.closeClick = function() {
+      $scope.windowOptions.visible = false;
   };
 
 };
